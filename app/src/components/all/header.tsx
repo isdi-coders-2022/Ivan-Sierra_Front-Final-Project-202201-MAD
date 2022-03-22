40a89f6c929141f90e8966257b17a444b800dd84
Ivan <canelo_1987_@hotmail.com>
2022-03-22 16:22:09 +0100

    Config files

---
+++ app/src/components/all/header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './all.scss';
+
+function Header(): JSX.Element {
+  return (
+    <div>
+      <nav>
+        <a href="# : string">Home</a>
+        <a href="# : string">Login/register</a>
+      </nav>
+
+      <h1>
+        FIX
+        <span>CAR</span>
+      </h1>
+    </div>
+  );
+}
+export default Header;
